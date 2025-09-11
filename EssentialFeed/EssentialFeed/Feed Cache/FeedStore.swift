c062902b537b1a2c4d68a438b558764fd7716b57
Andrés Carrillo <37129888+7andres@users.noreply.github.com>
2025-09-11 17:50:11 -0500

    Cache feed use case
    * Does not delete cache upon LocalFeedLoader creation
    
    * Save command requests cache deletion
    
    * Extract system under test creation to a factory method to protect tests against breaking changes
    
    * Move memory leak tracking extension from the Feed API module scope to a module-level shared scope
    
    * Add memory leak tracking
    
    * Save command does not request cache insertion on deletion error
    
    * Save command requests new cache insertion on successful deletion
    
    * Save command requests cache insertion with timestamp on successful deletion
    
    * Remove redundant test code
    
    * Unify store helper received messages to guarantee order and simplify test assertions
    
    * Save command fails on cache deletion error
    
    * Save command fails on cache insertion error
    
    * Save command succeeds on successful cache insertion
    
    * Extract duplicate test code into a shared helper method
    
    * Extract FeedStore protocol from test spy helper
    
    * Guarantee that the LocalFeedLoader does not deliver deletion error after instance has been deallocated
    
    * Guarantee that the LocalFeedLoader does not deliver insertion error after instance has been deallocated
    
    * Invert if logic to make code paths easier to follow
    
    * Extract cache insertion into a helper function to make logic inside closure callbacks easier to follow
    
    * Move LocalFeedLoader (and FeedStore collaborator) to its own file in production
    
    * Move FeedStore to its own file
    
    * Add SaveResult type alias to protect code from potential breaking changes

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Andrés Carrillo on 10/09/25.
+//
+
+import Foundation
+
+public protocol FeedStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+  
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,41 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Andrés Carrillo on 10/09/25.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+  private let store: FeedStore
+  private let currentDate: () -> Date
+  
+  public typealias SaveResult = Error?
+  
+  public init(store: FeedStore, currentDate: @escaping () -> Date) {
+    self.store = store
+    self.currentDate = currentDate
+  }
+  
+  public func save(_ items: [FeedItem], completion: @escaping (LocalFeedLoader.SaveResult) -> Void) {
+    store.deleteCachedFeed { [weak self] error in
+      guard let self else { return }
+      if let cacheDeletionError = error {
+        completion(cacheDeletionError)
+      } else {
+        cache(items, with: completion)
+      }
+    }
+  }
+  
+  private func cache (
+    _ items: [FeedItem],
+    with completion: @escaping (LocalFeedLoader.SaveResult) -> Void
+  ) {
+    store.insert(items, timestamp: currentDate()) { [weak self] error in
+      guard self != nil else { return }
+      completion(error)
+    }
+  }
+}
