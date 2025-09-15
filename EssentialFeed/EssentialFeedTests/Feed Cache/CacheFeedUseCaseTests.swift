a9eea6b5692701839b5063a64687fcf886f05abe
Andrés Carrillo <iamandres17@gmail.com>
2025-09-15 18:41:10 -0500

    Extract FeedCacheSpy into a shared scope to remove duplication

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -151,44 +151,6 @@ class CacheFeedUseCaseTests: XCTestCase {
     XCTAssertEqual(receivedError as NSError?, expectedError, file: file, line: line)
   }
   
-  private class FeedStoreSpy: FeedStore {
-    enum ReceivedMessage: Equatable {
-      case deleteCachedFeed
-      case insert([LocalFeedImage], Date)
-    }
-    
-    private(set) var receivedMessages = [ReceivedMessage]()
-    
-    private var deletionCompletions = [DeletionCompletion]()
-    private var insertionCompletions = [InsertionCompletion]()
-    
-    func completeDeletion(with error: Error, at index: Int = 0) {
-      deletionCompletions[index](error)
-    }
-    
-    func completeDeletionSuccessfully(at index: Int = 0) {
-      deletionCompletions[index](nil)
-    }
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-      deletionCompletions.append(completion)
-      receivedMessages.append(.deleteCachedFeed)
-    }
-    
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-      insertionCompletions.append(completion)
-      receivedMessages.append(.insert(feed, timestamp))
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = 0) {
-      insertionCompletions[index](error)
-    }
-    
-    func completeInsertionSuccessfully(at index: Int = 0) {
-      insertionCompletions[index](nil)
-    }
-  }
-  
   private func uniqueImage() -> FeedImage {
     return FeedImage(
       id: UUID(),
@@ -210,7 +172,7 @@ class CacheFeedUseCaseTests: XCTestCase {
     }
     return (models, local)
   }
-
+  
   private func anyURL() -> URL {
     return URL(string: "http://any-url.com")!
   }
